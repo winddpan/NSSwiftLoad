4f72b1be105e053f0594cce4d66c539162f6d4b8
winddpan <wind89@126.com>
2021-10-12 14:04:30 +0800

    Update NSSwiftLoader.m

--- NSSwiftLoader.m
+++ NSSwiftLoader.m
@@ -10,6 +10,7 @@
 #import <objc/runtime.h>
 #import <dlfcn.h>
 #import <mach-o/ldsyms.h>
+#import <mach-o/dyld.h>
 
 @interface NSSwiftLoader : NSObject
 @end
@@ -17,22 +18,44 @@
 @implementation NSSwiftLoader
 
 + (void)load {
-    int numClasses = 0, newNumClasses = objc_getClassList(NULL, 0);
-    Class *classes = NULL;
-    
-    while (numClasses < newNumClasses) { // 3
-        numClasses = newNumClasses;
-        classes = (Class *)realloc(classes, sizeof(Class) * numClasses);
-        newNumClasses = objc_getClassList(classes, numClasses);
-        
-        for (int i = 0; i < numClasses; i++) {
-            Class class = classes[i];
-            if (class_conformsToProtocol(class, @protocol(NSSwiftLoadProtocol))) {
-                [(id<NSSwiftLoadProtocol>)class swiftLoad];
+#if TARGET_IPHONE_SIMULATOR
+    uint32_t c = _dyld_image_count();
+    for(uint32_t i = 0; i < c; ++i) {
+        const char *image_name = _dyld_get_image_name(i);
+        NSString *imageName = [NSString stringWithFormat:@"%s", image_name];
+        if (![imageName containsString:@"/Platforms/"]) {
+            unsigned int classCount;
+            const char **classes;
+            classes = objc_copyClassNamesForImage(image_name, &classCount);
+            for (int i = 0; i < classCount; i++) {
+                Class class = objc_getClass(classes[i]);
+                if (class_conformsToProtocol(class, @protocol(NSSwiftLoadProtocol))) {
+                    [(id<NSSwiftLoadProtocol>)class swiftLoad];
+                }
             }
+            free(classes);
         }
     }
-    free(classes);
+#else
+    NSString *mainBundlePath = [NSBundle mainBundle].bundlePath;
+    uint32_t c = _dyld_image_count();
+    for(uint32_t i = 0; i < c; ++i) {
+        const char *image_name = _dyld_get_image_name(i);
+        NSString *imageName = [NSString stringWithFormat:@"%s", image_name];
+        if ([imageName hasPrefix:mainBundlePath]) {
+            unsigned int classCount;
+            const char **classes;
+            classes = objc_copyClassNamesForImage(image_name, &classCount);
+            for (int i = 0; i < classCount; i++) {
+                Class class = objc_getClass(classes[i]);
+                if (class_conformsToProtocol(class, @protocol(NSSwiftLoadProtocol))) {
+                    [(id<NSSwiftLoadProtocol>)class swiftLoad];
+                }
+            }
+            free(classes);
+        }
+    }
+#endif
 }
 
 @end
